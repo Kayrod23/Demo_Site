bfd0c47b9916a4991636e7b5b649797e93c4c396
Keeanu Rodriguez <keeanu7@gmail.com>
2023-10-26 12:15:28 -0400

    pokemon name

--- main.js
+++ main.js
@@ -10,4 +10,15 @@ function initialize () {
     }) 
 }
 
-initialize();
+const pkFrom = document.getElementById('pk-from');
+const pkName = document.getElementById('pk-form');
+
+pfForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pkName.value}`)
+    .then((res) => {
+        return res.json();
+    }).then((data) => {
+        console.log('Data =>', data)
+    }) 
+})
